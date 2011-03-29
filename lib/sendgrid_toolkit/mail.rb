f840243281371421a1e46c78b432278bc87f9a4d
Elad Meidar <elad@eizesus.com>
2011-03-29 17:53:21 +0200

    Fixing a small syntax error"

--- lib/sendgrid_toolkit/mail.rb
+++ lib/sendgrid_toolkit/mail.rb
@@ -1,7 +1,7 @@
 module SendgridToolkit
   class Mail < AbstractSendgridClient
     def send(options = {})
-      options.assert_valid_options :to :x-smtpapi, :subject, :from, :text, :html, :replyto, :from_name, :to_name, :date, :headers
+      options.assert_valid_options :to :x_smtpapi, :subject, :from, :text, :html, :replyto, :from_name, :to_name, :date, :headers
       response = api_post('mail', 'send', options)
       raise(SendEmailError, "SendMail API refused to send email: #{response["errors"].to_sentence}") if response["message"] = "error"
     end
