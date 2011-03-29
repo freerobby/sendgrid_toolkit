684262cd7c9f8ceda51e3019ce4f12dd0e488886
Elad Meidar <elad@eizesus.com>
2011-03-29 18:21:43 +0200

    debuggin prints

--- lib/sendgrid_toolkit/mail.rb
+++ lib/sendgrid_toolkit/mail.rb
@@ -1,8 +1,10 @@
 module SendgridToolkit
   class Mail < AbstractSendgridClient
     def send_mail(options = {})
-      options.assert_valid_keys :to, :x_smtpapi, :subject, :from, :text, :html, :reply_to, :from_name, :to_name, :date, :headers
+      options.assert_valid_keys :to, :api_headers, :subject, :from, :text, :html, :reply_to, :from_name, :to_name, :date, :headers
+      options["X-SMTPAPI"] = options[:api_headers]
       response = api_post('mail', 'send', options)
+      puts response.inspect
       raise(SendEmailError, "SendMail API refused to send email: #{response["errors"].to_sentence}") if response["message"] = "error"
     end
   end
