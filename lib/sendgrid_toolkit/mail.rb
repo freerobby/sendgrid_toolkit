5904aa3c3265e1eccb405ed200c5dd9f1200b57e
Elad Meidar <elad@eizesus.com>
2011-03-29 18:44:42 +0200

    Mailer class fix

--- lib/sendgrid_toolkit/mail.rb
+++ lib/sendgrid_toolkit/mail.rb
@@ -4,8 +4,7 @@ module SendgridToolkit
       options.assert_valid_keys :to, :api_headers, :subject, :from, :text, :html, :reply_to, :from_name, :to_name, :date, :headers
       options["X-SMTPAPI"] = options[:api_headers]
       response = api_post('mail', 'send', options)
-      puts response.inspect
-      raise(SendEmailError, "SendMail API refused to send email: #{response["errors"].to_sentence}") if response["message"] = "error"
+      raise(SendEmailError, "SendMail API refused to send email: #{response["errors"].inspect}") if response["message"] = "error"
     end
   end
 end
