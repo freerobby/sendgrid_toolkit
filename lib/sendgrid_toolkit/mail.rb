3adff0147a99b79fe9fb6575d630db926d038c37
Scott Moak <scott.moak@mybrainoncode.com>
2013-07-03 09:24:44 -0700

    Allow mail to send a post body

--- lib/sendgrid_toolkit/mail.rb
+++ lib/sendgrid_toolkit/mail.rb
@@ -1,7 +1,7 @@
 module SendgridToolkit
   class Mail < AbstractSendgridClient
-    def send_mail(options = {})
-      response = api_post('mail', 'send', options)
+    def send_mail(options = {}, body = {})
+      response = api_post('mail', 'send', options, body)
       raise(SendEmailError, "SendMail API refused to send email: #{response["errors"].inspect}") if response["message"] == "error"
       response
     end
