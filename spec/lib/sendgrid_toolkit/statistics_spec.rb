630fd54239c48c691acf03b6c7e328eea857ec0a
Michael Herold <michael.j.herold@gmail.com>
2014-01-28 14:38:29 -0500

    Add support for advanced statistics.

--- spec/lib/sendgrid_toolkit/statistics_spec.rb
+++ spec/lib/sendgrid_toolkit/statistics_spec.rb
@@ -48,4 +48,72 @@ describe SendgridToolkit::Statistics do
       cats[2]['category'].should == 'categoryC'
     end
   end
+
+  describe "#advanced" do
+    it "parses browser totals" do
+      FakeWeb.register_uri(:post, %r|https://#{REGEX_ESCAPED_BASE_URI}/stats\.getAdvanced\.json\?.*data_type=browsers|, :body => '[{"date":"2014-01-21","delivered":{"Chrome":1},"request":{"Chrome":1},"processed":{"Chrome":1}},{"date":"2014-01-22","delivered":{"Chrome":1},"request":{"Chrome":1},"processed":{"Chrome":1}}]')
+      stats = @obj.advanced('browsers', Date.new)
+      stats.each do |stat|
+        %w(delivered processed request).each do |type|
+          stat[type].kind_of?(Hash).should == true if stat.has_key?(type)
+        end
+        stat['date'].kind_of?(Date).should == true
+      end
+    end
+
+    it "parses client totals" do
+      FakeWeb.register_uri(:post, %r|https://#{REGEX_ESCAPED_BASE_URI}/stats\.getAdvanced\.json\?.*data_type=clients|, :body => '[{"date":"2014-01-21","delivered":{"Gmail":1},"request":{"Gmail":1},"processed":{"Gmail":1}},{"date":"2014-01-22","delivered":{"Gmail":1},"request":{"Gmail":1},"processed":{"Gmail":1}}]')
+      stats = @obj.advanced('clients', Date.new)
+      stats.each do |stat|
+        %w(delivered processed request).each do |type|
+          stat[type].kind_of?(Hash).should == true if stat.has_key?(type)
+        end
+        stat['date'].kind_of?(Date).should == true
+      end
+    end
+
+    it "parses device totals" do
+      FakeWeb.register_uri(:post, %r|https://#{REGEX_ESCAPED_BASE_URI}/stats\.getAdvanced\.json\?.*data_type=device|, :body => '[{"date":"2014-01-21","delivered":{"Webmail":1},"request":{"Webmail":1},"processed":{"Webmail":1}},{"date":"2014-01-22","delivered":{"Webmail":1},"request":{"Webmail":1},"processed":{"Webmail":1}}]')
+      stats = @obj.advanced('devices', Date.new)
+      stats.each do |stat|
+        %w(delivered processed request).each do |type|
+          stat[type].kind_of?(Hash).should == true if stat.has_key?(type)
+        end
+        stat['date'].kind_of?(Date).should == true
+      end
+    end
+
+    it "parses geo totals" do
+      FakeWeb.register_uri(:post, %r|https://#{REGEX_ESCAPED_BASE_URI}/stats\.getAdvanced\.json\?.*data_type=geo|, :body => '[{"date":"2014-01-21","delivered":{"US":1},"request":{"US":1},"processed":{"US":1}},{"date":"2014-01-22","delivered":{"US":1},"request":{"US":1},"processed":{"US":1}}]')
+      stats = @obj.advanced('geo', Date.new)
+      stats.each do |stat|
+        %w(delivered processed request).each do |type|
+          stat[type].kind_of?(Hash).should == true if stat.has_key?(type)
+        end
+        stat['date'].kind_of?(Date).should == true
+      end
+    end
+
+    it "parses global totals" do
+      FakeWeb.register_uri(:post, %r|https://#{REGEX_ESCAPED_BASE_URI}/stats\.getAdvanced\.json\?.*data_type=global|, :body => '[{"delivered":41,"request":41,"unique_open":1,"unique_click":1,"processed":41,"date":"2013-01-01","open":2,"click":1},{"delivered":224,"unique_open":1,"request":224,"processed":224,"date":"2013-01-02","open":3}]')
+      stats = @obj.advanced('global', Date.new)
+      stats.each do |stat|
+        %w(click delivered open processed request unique_click unique_open).each do |type|
+          stat[type].kind_of?(Integer).should == true if stat.has_key?(type)
+        end
+        stat['date'].kind_of?(Date).should == true
+      end
+    end
+
+    it "parses ISP totals" do
+      FakeWeb.register_uri(:post, %r|https://#{REGEX_ESCAPED_BASE_URI}/stats\.getAdvanced\.json\?.*data_type=isps|, :body => '[{"date":"2014-01-21","delivered":{"Gmail":1},"request":{"Gmail":1},"processed":{"Gmail":1}},{"date":"2014-01-22","delivered":{"Gmail":1},"request":{"Gmail":1},"processed":{"Gmail":1}}]')
+      stats = @obj.advanced('isps', Date.new)
+      stats.each do |stat|
+        %w(delivered processed request).each do |type|
+          stat[type].kind_of?(Hash).should == true if stat.has_key?(type)
+        end
+        stat['date'].kind_of?(Date).should == true
+      end
+    end
+  end
 end
