589ebef3e5111087da8c3772f7101ccbde1a1d46
Richard Lee <dlackty@gmail.com>
2014-04-01 16:45:03 +0800

    Allow customizing base uri

--- spec/lib/sendgrid_toolkit_spec.rb
+++ spec/lib/sendgrid_toolkit_spec.rb
@@ -1,4 +1,23 @@
 require File.expand_path("#{File.dirname(__FILE__)}/../helper")
 
 describe SendgridToolkit do
+  subject { described_class }
+  describe ".base_uri" do
+    it "returns value set by user" do
+      subject.base_uri = "foo"
+      subject.base_uri.should == "foo"
+    end
+
+    it "finds ENV variable" do
+      subject.base_uri = nil
+      ENV['SENDGRID_API_BASE_URI'] = "bar"
+      subject.base_uri.should == "bar"
+    end
+
+    it "returns default value" do
+      subject.base_uri = nil
+      ENV['SENDGRID_API_BASE_URI'] = nil
+      subject.base_uri.should == subject::BASE_URI
+    end
+  end
 end
