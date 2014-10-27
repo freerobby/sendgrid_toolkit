31cb188e0d80ee157a9cfa4acf8f323d56d9703f
Chris Hobbs <purposemc@gmail.com>
2014-10-27 12:39:12 -0700

    Actually post data in api_post instead of putting it all on the query string.

--- lib/sendgrid_toolkit/abstract_sendgrid_client.rb
+++ lib/sendgrid_toolkit/abstract_sendgrid_client.rb
@@ -14,7 +14,7 @@ module SendgridToolkit
     def api_post(module_name, action_name, opts = {})
       base_path = compose_base_path(module_name, action_name)
       response = HTTParty.post("https://#{SendgridToolkit.base_uri}/#{base_path}.json?",
-                               :query => get_credentials.merge(opts),
+                               :body => get_credentials.merge(opts),
                                :format => :json)
       if response.code > 401
         raise(SendgridToolkit::SendgridServerError, "The sengrid server returned an error. #{response.inspect}")
